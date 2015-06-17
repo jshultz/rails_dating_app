487af8b78d1d0513eb4bbbd3b0ed9c283b49b0f3
Jason Shultz <jasshultz@gmail.com>
2015-06-17 07:59:31 -0600

    update profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,5 @@
 class UsersController < ApplicationController
 
-  def show
-    @user = User.find(params[:id])
-    gender = @user == 0 ? 1 : 0
-    @people = User.where.not(id: @user.id, gender: gender)
-  end
-
-  def new
-    @user = User.new
-  end
-
   def create
     @user = User.new(user_params)
     if @user.save
@@ -21,11 +11,36 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find current_user.id
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+    gender = @user == 0 ? 1 : 0
+    @people = User.where.not(id: @user.id, gender: gender)
+  end
+
+  def update
+    @user = User.find current_user.id
+    if @user.update(user_params)
+      flash[:success] = 'Your profile was updated'
+      redirect_to @user
+    else
+      flash[:error] = 'Your profile was not updated. There was errors.'
+      render 'edit'
+    end
+  end
+
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :gender, :password,
-                                   :password_confirmation)
+                                   :password_confirmation, :city, :birth_date)
     end
 
 
